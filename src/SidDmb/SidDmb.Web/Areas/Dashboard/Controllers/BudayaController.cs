072a206a6efa2829e7fa4be1715c3994182dc271
Lavitzz <allgaming1185@gmail.com>
2024-12-12 14:48:07 +0800

    daftar artefak budaya

---
+++ src/SidDmb/SidDmb.Web/Areas/Dashboard/Controllers/BudayaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SidDmb.Web.Areas.Dashboard.Controllers;
+[Area("Dashboard")]
+public class BudayaController : Controller
+{
+    public IActionResult ArtefakBudaya()
+    {
+        return View();
+    }
+}
