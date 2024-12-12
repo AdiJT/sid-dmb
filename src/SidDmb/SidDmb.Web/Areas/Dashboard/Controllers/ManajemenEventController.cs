970487db633d83fe7ef885f5c6fed658f02df16a
Lavitzz <allgaming1185@gmail.com>
2024-12-12 18:45:34 +0800

    daftar Pelaporan & Dokumentasi

---
+++ src/SidDmb/SidDmb.Web/Areas/Dashboard/Controllers/ManajemenEventController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SidDmb.Web.Areas.Dashboard.Controllers;
+[Area("Dashboard")]
+public class ManajemenEventController : Controller
+{
+    public IActionResult PelaporanDokumentasi()
+    {
+        return View();
+    }
+}
