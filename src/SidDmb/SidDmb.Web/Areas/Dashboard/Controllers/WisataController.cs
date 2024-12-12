b71ff2bfd0248a6bb031545c4cc6beacf383006c
Lavitzz <allgaming1185@gmail.com>
2024-12-12 13:58:37 +0800

    sedikit

---
+++ src/SidDmb/SidDmb.Web/Areas/Dashboard/Controllers/WisataController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SidDmb.Web.Areas.Dashboard.Controllers;
+
+[Area("Dashboard")]
+public class WisataController : Controller
+{
+    public IActionResult DestinasiWisata()
+    {
+        return View();
+    }
+}
