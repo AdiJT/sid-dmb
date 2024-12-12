f5c9ac8bd88e557e5198382638f627cb83e13a73
Lavitzz <allgaming1185@gmail.com>
2024-12-12 15:11:33 +0800

    daftar Poduk Lokal

---
+++ src/SidDmb/SidDmb.Web/Areas/Dashboard/Controllers/PreneurController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SidDmb.Web.Areas.Dashboard.Controllers;
+[Area("Dashboard")]
+public class PreneurController : Controller
+{
+    public IActionResult ProdukLokal()
+    {
+        return View();
+    }
+}
