22937d78264a763e10dd461a8300b8bd6b582816
Lavitzz <allgaming1185@gmail.com>
2024-12-12 15:30:55 +0800

    daftar Kegiatan Prima

---
+++ src/SidDmb/SidDmb.Web/Areas/Dashboard/Controllers/PrimaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SidDmb.Web.Areas.Dashboard.Controllers;
+[Area("Dashboard")]
+public class PrimaController : Controller
+{
+    public IActionResult KegiatanPrima()
+    {
+        return View();
+    }
+}
