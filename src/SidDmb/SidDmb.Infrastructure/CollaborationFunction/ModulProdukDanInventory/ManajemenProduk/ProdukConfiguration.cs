b26fd8ccbf2c831bffedfc956993e322a1b2f173
AdiJT <aditaklal@gmail.com>
2024-12-29 10:25:19 +0800

    Sedikit

--- src/SidDmb/SidDmb.Infrastructure/CollaborationFunction/ModulProdukDanInventory/ManajemenProduk/ProdukConfiguration.cs
+++ src/SidDmb/SidDmb.Infrastructure/CollaborationFunction/ModulProdukDanInventory/ManajemenProduk/ProdukConfiguration.cs
@@ -14,5 +14,24 @@ internal class ProdukConfiguration : IEntityTypeConfiguration<Produk>
 
         builder.HasMany(x => x.DaftarDistribusi).WithOne(x => x.Produk);
         builder.HasMany(x => x.DaftarKolaborator).WithMany(x => x.DaftarProduk);
+
+        builder.HasData(
+            new Produk
+            {
+                Id = IdProduk.Create("MP001").Value,
+                Nama = "Batik Tulis Gilang",
+                Dekripsi = "Batik tulis dengan pewarna alami dan motif khas Desa Gilangharjo.",
+                Kategori = KategoriProduk.Kerajinan,
+                UnitUsahaTerkait = "Batik Gilang Sejahtera",
+                HargaProduk = 150_000d,
+                StokAwal = 100,
+                StokSaatIni = 50,
+                StatusKetersediaan = StatusKetersediaan.Tersedia,
+                TanggalProduksiTerakhir = new DateOnly(2024, 12, 1),
+                TanggalKadaluarsa = new DateOnly(2024, 12, 1),
+                LegalitasProduk = "PIRT 12345/2024, Halal MUI",
+                MediaPromosi = new Uri("/assets/produklokal2-test.png", UriKind.Relative),
+            }
+        );
     }
 }
---
+++ src/SidDmb/SidDmb.Infrastructure/Migrations/20241229022333_TambahDataProduk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SidDmb.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TambahDataProduk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Produk",
+                columns: new[] { "Id", "Dekripsi", "HargaProduk", "Kategori", "LegalitasProduk", "MediaPromosi", "Nama", "StatusKetersediaan", "StokAwal", "StokSaatIni", "TanggalDiinputkan", "TanggalKadaluarsa", "TanggalPembaruanData", "TanggalProduksiTerakhir", "UnitUsahaTerkait" },
+                values: new object[] { "MP001", "Batik tulis dengan pewarna alami dan motif khas Desa Gilangharjo.", 150000.0, 0, "PIRT 12345/2024, Halal MUI", "/assets/produklokal2-test.png", "Batik Tulis Gilang", 0, 100, 50, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateOnly(2024, 12, 1), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateOnly(2024, 12, 1), "Batik Gilang Sejahtera" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Produk",
+                keyColumn: "Id",
+                keyValue: "MP001");
+        }
+    }
+}
